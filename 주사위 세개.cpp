49ebe62f82587a9bbff34b2d7e206570c4157cdc
jhl9617 <57484647+jhl9617@users.noreply.github.com>
2022-09-27 23:41:23 +0900

    220927

--- 주사위 세개.cpp
+++ 주사위 세개.cpp
@@ -82,11 +82,14 @@ using namespace std;
 int main(){
     int a, b, c;
     cin >> a >> b >> c;
-    int result;
-    if(a == b && a == c)result = 10000 + a * 1000;
+    int result = 0;
+    if(a == b && a == c){
+        result = 10000 + a * 1000;}
     else if((a == b || a == c) || b == c) {
-        if( a == b)result = 1000 + a * 100;
-        if( c == b)result = 1000 + c * 100;
+        if( a == b){
+            result = 1000 + a * 100;}
+        if( c == b){
+            result = 1000 + c * 100;}
     }
     else result = max(max(a, b), c) * 100;
 
