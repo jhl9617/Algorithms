9b39ae873784af7037e7eef5edd8a2c9d35021c8
jonghalee <jhl9617@gmail.com>
2022-12-28 17:18:48 +0900

    2022.12.28

--- baekjoon/16. 백트래킹/N과 M (1).py
+++ baekjoon/16. 백트래킹/N과 M (1).py
@@ -1,9 +1,13 @@
-from itertools import combinations
+from itertools import product
 
-A, B = map(int, input().split())
-nums = []
-for i in range(1, A+1):
-    nums.append(i)
-nums = combinations(nums, B)
-for i in nums:
-    print(i)
+N, M = map(int, input().split())
+
+ans = []
+
+def back():
+    if len(ans) == M:   #배열의 길이를 확인
+        print(" ".join(map(str, ans)))  #1 2 3 이런 상태로 출력
+        return
+    for i in range(1, N + 1):
+        if i not in ans:
+            ans.append(i)
--- test.py
+++
@@ -1,3 +0,0 @@
-num = 5
-for i in range(0, 10, 3):
-    print(i)
